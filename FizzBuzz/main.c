0da9b595b3d0996fab84ea637b85b8194f059fae
Graeme Harrison <gharrison365@gmail.com>
2015-12-20 17:00:47 -0500

    Completed FizzBuzz project.

--- FizzBuzz/main.c
+++ FizzBuzz/main.c
@@ -2,14 +2,30 @@
 //  main.c
 //  FizzBuzz
 //
-//  Created by Graeme Harrison on 2015-12-20.
+//  Created by Graeme Harrison on 2015-12-16.
 //  Copyright © 2015 Graeme Harrison. All rights reserved.
 //
+//Write a program that prints the numbers from 1 to 100. For the multiples of 3, print "Fizz" instead of the number and for the multiples of 5, print "Buzz" instead of the number. For numbers which are multiples of both 3 and 5 print "FizzBuzz"
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int x = 1;
+    while (x <=100)
+    {
+        if (x % 3 == 0 && x % 5 == 0) {
+            printf("FizzBuzz \n");
+            x++;
+        } else if (x % 3 == 0) {
+            printf("Fizz \n");
+            x++;
+        } else if (x % 5 == 0) {
+            printf("Buzz \n");
+            x++;
+        } else {
+            printf("%d\n", x);
+            x++;
+        }
+    }
     return 0;
 }
